3155ea9218efcf9e20330956711a5123bec1fb1b
Thiago Ricardo <t.ricardooliveira08@gmail.com>
2025-03-06 23:55:00 +0000

    adding division and power functions

--- calculator.py
+++ calculator.py
@@ -1,5 +1,12 @@
+# Calculator - Group 5
+
 import math
 
-def input_treat(input):
-    if input != NULL:
-        
+def power(base: float, exponent: float) -> float:
+    return math.pow(base, exponent)
+
+def division(numerator: float, denominator: float) -> float:
+    if denominator == 0:
+        raise ZeroDivisionError("Erro: divisão por zero não permitida.")
+    return numerator / denominator
+
