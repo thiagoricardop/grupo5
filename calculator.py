9684f66708c045c913ebcc496aefe0bd94bd92cc
pedrolucena05 <pedrolucena0523@gmail.com>
2025-03-06 21:07:43 -0300

    Adicionando funções de soma e subtração

--- calculator.py
+++ calculator.py
@@ -1,6 +1,9 @@
 # Calculator - Group 5
+import sum
+import subtract
 
 import math
+
 
 def power(base: float, exponent: float) -> float:
     return math.pow(base, exponent)
@@ -9,4 +12,14 @@ def division(numerator: float, denominator: float) -> float:
     if denominator == 0:
         raise ZeroDivisionError("Erro: divisão por zero não permitida.")
     return numerator / denominator
+
+def sum (x, y):
+    return x + y
+
+def sub (x, y):
+    return x - y
+
+
+
+
 
